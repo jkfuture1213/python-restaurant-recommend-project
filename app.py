f5a6b5ce2531533babc285abaf37a162d4ae878b
jkfuture1213 <jkfuture1213@gmail.com>
2026-07-29 12:19:09 +0900

    bug fix

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ if random_button:
     <html>
     <head>
     <meta charset="utf-8">
-    <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JS_KEY}&autoload=false&libraries=services"></script>
+    <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JS_KEY}&autoload=false"></script>
     </head>
     
     <body>
@@ -185,7 +185,7 @@ if search_button:
     <html>
     <head>
     <meta charset="utf-8">
-    <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JS_KEY}&autoload=false&libraries=services"></script>
+    <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={KAKAO_JS_KEY}&autoload=false"></script>
     </head>
     
     <body>
